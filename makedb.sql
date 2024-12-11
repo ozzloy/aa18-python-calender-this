2cfb5de75e55bf711d27665dfb46341f7f82d54d
daniel watson <ozzloy@each.do>
2024-12-11 13:20:49 -0800

    separate table creation and seeding

--- makedb.sql
+++ makedb.sql
@@ -1,23 +1,8 @@
-CREATE TABLE appointments (
+CREATE TABLE IF NOT EXISTS appointments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(200) NOT NULL,
   start_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   end_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   description TEXT NOT NULL,
   private BOOLEAN NOT NULL
-);
-
-INSERT INTO appointments (
-    name,
-    start_datetime,
-    end_datetime,
-    description,
-    private
-)
-VALUES (
-    'My appointment',
-    '2024-12-11 14:00:00',
-    '2024-12-11 15:00:00',
-    'An appointment for me',
-    false
 );
---
+++ seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO appointments (
+    name,
+    start_datetime,
+    end_datetime,
+    description,
+    private
+)
+VALUES (
+    'My appointment',
+    '2024-12-11 14:00:00',
+    '2024-12-11 15:00:00',
+    'An appointment for me',
+    false
+);
