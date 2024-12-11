c833f55fedf5d8bcdf8988b3a083079309cc927f
daniel watson <ozzloy@each.do>
2024-12-11 12:39:43 -0800

    make template grab info from db

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,29 @@
+import os
+import sqlite3
 from flask import Blueprint, render_template
+from datetime import datetime
+
 
 bp = Blueprint("main", __name__, "/")
 
 
+DB_FILE = os.environ.get("DB_FILE") or "set DB_FILE in .env"
+
+
 @bp.route("/")
 def main():
-    return render_template("main.html")
+    with sqlite3.connect(DB_FILE) as conn:
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+        SELECT id, name, start_datetime, end_datetime
+        FROM appointments
+        ORDER BY start_datetime;
+        """
+        )
+        rows = cur.fetchall()
+
+        return render_template(
+            "main.html", rows=rows, datetime=datetime
+        )
