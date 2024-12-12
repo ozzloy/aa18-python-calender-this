7633da86502581dd19e76ab1c659912fbffa0295
daniel watson <ozzloy@each.do>
2024-12-11 16:35:31 -0800

    display appointments only for the current day

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
-from flask import Blueprint, render_template, redirect
-from datetime import datetime
+from flask import Blueprint, render_template, redirect, url_for
+from datetime import datetime, timedelta
 
 from app.forms import AppointmentForm
 
@@ -52,15 +52,23 @@ def handle_valid_form(conn, form):
     return redirect("/")
 
 
-def show_appointments(conn, form):
+def show_appointments(conn, form, year, month, day):
     cur = conn.cursor()
 
+    day = datetime(year, month, day)
+    next_day = day + timedelta(days=1)
+    params = {
+        "day": day,
+        "next_day": next_day,
+    }
     cur.execute(
         """
         SELECT id, name, start_datetime, end_datetime
         FROM appointments
+        WHERE start_datetime BETWEEN :day AND :next_day
         ORDER BY start_datetime;
-        """
+        """,
+        params,
     )
     rows = cur.fetchall()
 
@@ -77,11 +85,22 @@ def show_appointments(conn, form):
     return render_template("main.html", rows=rows, form=form)
 
 
-@bp.route("/", methods=["GET", "POST"])
-def main():
+@bp.route(
+    "/<int:year>/<int:month>/<int:day>", methods=["GET", "POST"]
+)
+def daily(year, month, day):
     form = AppointmentForm()
     with sqlite3.connect(DB_FILE) as conn:
         if form.validate_on_submit():
             return handle_valid_form(conn, form)
         else:
-            return show_appointments(conn, form)
+            return show_appointments(conn, form, year, month, day)
+
+
+@bp.route("/", methods=["GET", "POST"])
+def main():
+    now = datetime.now()
+    year, month, day = (now.year, now.month, now.day)
+    return redirect(
+        url_for(".daily", year=year, month=month, day=day)
+    )
